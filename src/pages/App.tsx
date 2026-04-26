72ff746c175df4f9fba0a901f5686559e196c548
ninefadacai888-collab <ninefadacai888@gmail.com>
2026-04-27 00:31:02 +0800

    Add routing for main application pages

---
+++ src/pages/App.tsx
@@ -0,0 +1,8 @@
+<Route path="/" element={<Index />} />
+<Route path="/about" element={<About />} />
+<Route path="/shop" element={<Shop />} />
+<Route path="/shop/:id" element={<ProductDetail />} />
+<Route path="/portfolio" element={<Portfolio />} />
+<Route path="/custom" element={<Custom />} />
+<Route path="/cart" element={<Cart />} />
+<Route path="/checkout" element={<Checkout />} />
