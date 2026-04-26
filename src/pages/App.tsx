38908ce3ae03581238ecbcdff18572813c83d7d8
ninefadacai888-collab <ninefadacai888@gmail.com>
2026-04-27 00:34:10 +0800

    Delete src/pages/App.tsx

--- src/pages/App.tsx
+++
@@ -1,8 +0,0 @@
-<Route path="/" element={<Index />} />
-<Route path="/about" element={<About />} />
-<Route path="/shop" element={<Shop />} />
-<Route path="/shop/:id" element={<ProductDetail />} />
-<Route path="/portfolio" element={<Portfolio />} />
-<Route path="/custom" element={<Custom />} />
-<Route path="/cart" element={<Cart />} />
-<Route path="/checkout" element={<Checkout />} />
